2a1aca044cae64ebcfa15490b9527c9bb6bebfb4
jesse sheehan <jessetsheehan@gmail.com>
2022-07-04 12:51:46 -0400

    finished all queries for express, and commented them out

--- server.js
+++ server.js
@@ -22,9 +22,42 @@ const db = mysql.createConnection(
 //db runs the query method, which means it runs the query 
 //in the parentheses, and executes a callback with 
 //resulting row that match the query
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
-});
+
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+// });
+
+//get a SINGLE candidate - note how "rows" is now "row"
+
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if (err) {
+//         console.log(err);
+//     } 
+//     console.log(row);
+// });
+
+//delete a candidate
+
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     } 
+//     console.log(result);
+// });
+
+//create a candidate
+
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//     VALUES (?, ?, ?, ?)`;
+// const params = [1, 'Ronald', "Firbank", 1]   ;
+
+// db.query(sql, params, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// })
+
 
 //default response for any other request (anything Not Found)
 app.use((req, res) => {
